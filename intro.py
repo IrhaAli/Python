2996669c995e798b28c4f16565745079412422c0
Irha Ali <irhaali_97@hotmail.ca>
2023-04-24 12:17:42 -0400

    More string operations

--- intro.py
+++ intro.py
@@ -7,6 +7,8 @@ print('Hello, ' + name)
 # Fundamental Data Types
 # int (whole numbers)
 print(type(2+4))
+# converting to int
+int('100')
 
 # float (decimal numbers). float takes up more space than int
 print(type(2/4))
@@ -28,6 +30,29 @@ first_name = 'Irha'
 last_name = 'Ali'
 full_name = first_name + ' ' + last_name
 print(full_name)
+
+# Converting to string
+str(100)
+
+# Escape sequences (\, \t adds tab, \n adds line)
+weather = 'It\'s sunny'
+
+# Formatted string
+print(f'Hi, my name is {first_name} {last_name}.')
+print('Hi, my name is {} {}.'.format('Irha', 'Ali'))
+print('Hi, my name is {1}, {0}.'.format('Irha', 'Ali'))
+print('Hi, my name is {new_first_name} {new_last_name}.'.format(new_first_name = 'Pablo', new_last_name = 'Escobar'))
+
+# Subsdtring
+selfish = '01234567'
+# [start:stop:stepover]
+print(selfish[0:8])
+print(selfish[0:])
+print(selfish[:8])
+print(selfish[0:8:2])
+print(selfish[-1])
+# Reverse the string
+print(selfish[::-1])
 
 # list
 # tuple
