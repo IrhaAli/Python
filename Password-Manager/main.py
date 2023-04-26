55b934b83c025a3c8b8c9b1abbb2a286ebf8ad99
Irha Ali <irhaali_97@hotmail.ca>
2023-04-26 13:50:29 -0400

    Complete password manager

--- Password-Manager/main.py
+++ Password-Manager/main.py
@@ -1,35 +1,54 @@
-pwd = input("What is the master password? ")
-master_password = "123"
+from cryptography.fernet import Fernet
 
-if pwd != master_password:
-    print("Incorrect master password")
-    quit()
+# Key for encryption
+'''
+def write_key():
+   key = Fernet.generate_key()
+   with open("key.key", "wb") as key_file:
+      key_file.write(key)
 
+write_key()
+'''
+
+# Encryption/Decryption Settings
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
+
+# View all passwords
 def view():
-  with open('password.txt', 'r') as f:
-    for line in f.readlines():
-       data = line.rstrip()
-       user, passw = data.split("|")
-       print("User:", user + ",", "Password:", passw)
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 
+# Add a password
 def add():
-    name = input("Account Name: ")
+    name = input('Account Name: ')
     pwd = input("Password: ")
 
     # This way it automatically closes the file
-    with open('password.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
-        
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
+# Deal with user input to view, add or quit
 while True:
-  mode = input("Would you like to add a new password or view existing ones or quit (a/v/q)? ")
+    mode = input(
+        "Would you like to add a new password or view existing ones or quit (a/v/q)? ")
 
-  if mode == "q":
-      break
-  elif mode == "a":
-      add()
-  elif mode == "v":
-      view()
-  else:
-      print("Invalid mode.")
-      continue
+    if mode == "q":
+        break
+    elif mode == "a":
+        add()
+    elif mode == "v":
+        view()
+    else:
+        print("Invalid mode.")
+        continue
