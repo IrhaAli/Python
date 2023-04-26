811d23cffdb6848a07b623d4c0e746766cbfb77a
Irha Ali <irhaali_97@hotmail.ca>
2023-04-26 16:18:35 -0400

    Complete data types learning

--- intro.py
+++ intro.py
@@ -2,9 +2,9 @@
 name = input('What is your name? ')
 print('Hello, ' + name)
 
-# Terms
+############################### Terms ##############################
 
-# Fundamental Data Types
+############################## Fundamental Data Types ##############################
 # int (whole numbers)
 print(type(2+4))
 # converting to int
@@ -45,7 +45,8 @@ weather = 'It\'s sunny'
 print(f'Hi, my name is {first_name} {last_name}.')
 print('Hi, my name is {} {}.'.format('Irha', 'Ali'))
 print('Hi, my name is {1}, {0}.'.format('Irha', 'Ali'))
-print('Hi, my name is {new_first_name} {new_last_name}.'.format(new_first_name = 'Pablo', new_last_name = 'Escobar'))
+print('Hi, my name is {new_first_name} {new_last_name}.'.format(
+    new_first_name='Pablo', new_last_name='Escobar'))
 
 # Subsdtring [start:stop:stepover]
 selfish = '01234567'
@@ -70,30 +71,41 @@ list2 = list[:]
 # instead of
 list2 = list
 # List methods (put . at the end of the list variable)
-basket = [1,2,3,4,5,'b']
+basket = [1, 2, 3, 4, 5, 'b']
 # Adding
 basket.extend([100])
 # Removing
 basket.pop()
 # Get intex
 basket.index('b')
-'b' in basket # returns True or False
+'b' in basket  # returns True or False
 basket.count('b')
-basket.sort() # sorts the list (modies original list)
-sorted(basket) # sorts the list (creates new list)
-basket.reverse() # reverse the list (modies original list)
-a,b,c, *other, d = [1,2,3,4,5,6,7,8]
+basket.sort()  # sorts the list (modies original list)
+sorted(basket)  # sorts the list (creates new list)
+basket.reverse()  # reverse the list (modies original list)
+a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8]
 print(other)
 
 # tuple (basically immutable list)
 # They're faster than list
-my_tuple = (1,2,3,4,5)
+my_tuple = (1, 2, 3, 4, 5)
 print(my_tuple)
 print(my_tuple[1])
 print(5 in my_tuple)
 
-# set
-
+# set (unordered collection of unique objects)
+# It does not support indexing
+my_set = {1, 2, 3, 4, 5, 5}
+print(my_set)  # {1, 2, 3, 4, 5}
+my_list = [1, 2, 3, 4, 5, 5]
+print(set(my_list))
+# .difference: difference between two sets
+# .discard: removes elements from the set
+# .difference_update: the intersection of two sets is removed
+# intersection: gives the intersection of two sets
+# isdisjoint: true if the two sets are disjoint else false
+# issubset: checks if the one set is subset of the other
+# issuperset: checks if the one set is superset of the other
 
 # dict (like object in Javascript)
 # The dictionary key needs to be a data type that is immutable
@@ -103,13 +115,12 @@ dictionary = {
     'b': 2
 }
 
-
 # Variables (Note: must be in snake_case.
 # _ at the beginning indicates private variables)
 # Don't use keywords in python as a variable name
 # all caps variable indicate the variable is a constant
 # How to assign multiple variables values
-a,b,c = 1,2,3
+a, b, c = 1, 2, 3
 print(a, b, c)
 
 # Classes (custom types)
@@ -120,7 +131,7 @@ print(a, b, c)
 # When you wanna initialize a variable but not assign it a value
 weapons = None
 
-# Actions 
+############################### Actions ##############################
 # (operations: +, - , *, /, **, // (round down when dividing),
 # %, round, abs, etc)
 # Operator precedence (ie. BEDMAS)
@@ -132,11 +143,11 @@ print(int('0b101', 2))
 
 # Augmented assignment operator
 some_value = 5
-some_value += 2 # instead of some_value = some_value + 2
+some_value += 2  # instead of some_value = some_value + 2
 print(some_value)
 
 # Built-in methods (functions only used by a certain data type)
 # Eg. Length of a string
 print('Lenght of a string', len('1234567'))
 
-# Best Practices
+############################### Best Practices ##############################
